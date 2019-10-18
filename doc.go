7a142da72e8b1c9f13a8cb9c59e17fab94fece5c
bytemare <{ID}+{username}@users.noreply.github.com>
2019-10-18 16:34:07 +0200

    updated

--- doc.go
+++ doc.go
@@ -2,9 +2,11 @@
 Package crawl is a simple link scraper and web crawler with single domain scope.
 It can be limited with a timeout and interrupted with signals.
 
-Three public functions give access to single page link scraping (ScrapLinks) and single host web crawling (FetchLinks and StreamLinks).
+Three public functions give access to single page link scraping (ScrapLinks) and
+single host web crawling (FetchLinks and StreamLinks).
 FetchLinks and StreamLinks have the same behaviour and result, as FetchLinks is a wrapper for StreamLinks.
-The only difference is that FetchLinks is blocking, and returns once a stopping condition is reached (link tree exhaustion, timeout, signals),
+The only difference is that FetchLinks is blocking,
+and returns once a stopping condition is reached (link tree exhaustion, timeout, signals),
 where StreamLinks immediately returns a channel on which the calling function can listen on to get results as they come.
 
 The return values can be used for a site map.
